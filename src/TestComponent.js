68701ec3441447a3fd72bc2a33b0efa1f5cc535b
Mohamad Farahmandifard <ticticb80om@gmail.com>
2019-10-30 13:35:24 -0600

    Done.

--- src/TestComponent.js
+++ src/TestComponent.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 
+
 // no need to modify anything in here. The final test is looking for the returned JSX from this component
 class TestComponent extends Component {
   render() {
